f6d67972ae0bd2f79bd7761760a77817572d0acf
Itzá Gabriela <99753184+gabbza@users.noreply.github.com>
2024-01-10 14:08:44 -0600

    Create Main.java

---
+++ POO/clases/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main (String[] args) {
+        parametros object = new parametros();
+        object.visualizacion1();
+        object.visualizacion2(24);
+    }
+}
