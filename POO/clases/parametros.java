cab9785af711da23e719ddc28099b1059ac137ed
Itzá Gabriela <99753184+gabbza@users.noreply.github.com>
2024-01-10 14:10:07 -0600

    Create parametros.java

---
+++ POO/clases/parametros.java
@@ -0,0 +1,11 @@
+class parametros{
+    public void visualizacion1(){
+        System.out.println("Método sin parámetro");
+    }
+    public void visualizacion2(int a){
+        System.out.println("Método con un único parámetro: " + a);
+        
+    }
+
+
+}
